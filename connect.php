5e7e69c5e5c3830ddf64140f2f813b2816086a03
AshwinT2727 <you@example.com>
2024-01-15 22:43:58 +0530

    attempt to establish db connection

--- connect.php
+++ connect.php
@@ -1,40 +1,37 @@
 <?php
-$name = filter_input(INPUT_POST, 'name');
-$fathername = filter_input(INPUT_POST, 'fathername');
-$address = filter_input(INPUT_POST, 'address');
-$DOB = filter_input(INPUT_POST, 'DOB');
-$buisnessname = filter_input(INPUT_POST, 'buisnessname');
-$natureofbuisness = filter_input(INPUT_POST, 'natureofbuisness');
-$pan = filter_input(INPUT_POST, 'pan');
-$cellno = filter_input(INPUT_POST, 'cellno');
-$financialyear = filter_input(INPUT_POST, 'financialyear');
-$assesmentyear = filter_input(INPUT_POST, 'assesmentyear');
-$bankacno = filter_input(INPUT_POST, 'bankacno');
-$ifsc = filter_input(INPUT_POST, 'ifsc');
-$email = filter_input(INPUT_POST, 'email');
-$uidno = filter_input(INPUT_POST, 'uidno');
-
-$host = "localhost";
-$dbusername = "root";
-$dbpassword = ""; 
-$dbname = "ca_project";
+$name = $_POST['name'];
+$fathername = $_POST['fathername'];
+$address = $_POST['address'];
+$dob = $_POST['dob'];
+$nameofbuisness = $_POST['nameofbuisness'];
+$natureofbuisness = $_POST['natureofbuisness'];
+$pannnumber = $_POST['pannnumber'];
+$phonenumber = $_POST['phonenumber'];
+$financialyear = $_POST['financialyear'];
+$assessmentyear = $_POST['assessmentyear'];
+$bankaccnumber = $_POST['bankaccnumber'];
+$ifsc = $_POST['ifsc'];
+$email = $_POST['email'];
+$uidnumber = $_POST['uidnumber'];
 
 // Create connection
-$conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+$conn = new mysqli('localhost', 'root', '', 'caproject');
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+else
+{
 
-$sql = "INSERT INTO details (name, father_name, address, dob, name_of_buisness, nature_of_buisness, pan_number, phone_number, financial_year, assessment_year, bank_acc_number, ifsc, email, uid_number)
-        VALUES ('$name', '$fathername', '$address', '$DOB', '$buisnessname', '$natureofbuisness', '$pan', '$cellno', '$financialyear', '$assesmentyear', '$bankacno', '$ifsc', '$email', '$uidno')";
 
-if ($conn->query($sql) === TRUE) {
-    echo "Record inserted successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+$stmt =  $conn->prepare("INSERT INTO details (name, fathername, address, dob, nameofbuisness, natureofbuisness, pannnumber, phonenumber, financialyear, assessmentyear, bankaccnumber, ifsc, email, uidnumber)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+    $stmt->bind_param("sssssssssssss", $name, $fathername, $address, $dob, $nameofbuisness, $natureofbuisness, $pannnumber, $phonenumber, $financialyear, $assessmentyear, $bankaccnumber, $ifsc, $email, $uidnumber);
+    $stmt->execute();
+        echo "Record inserted successfully";
+        $stmt->close();
+        $conn->close();
 }
-
-$conn->close();
 ?>
